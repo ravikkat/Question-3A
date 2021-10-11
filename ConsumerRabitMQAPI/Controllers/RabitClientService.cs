14887aa50c56e5c44f47c4e7343a5666c71eb8ce
pom-user <pom-user@ST-POM-LAB-NB41.ARES.shared-svc.local>
2021-10-11 18:40:32 +0800

    rabit producet 3A

---
+++ ConsumerRabitMQAPI/Controllers/RabitClientService.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Text;
+using System.Threading;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+namespace ConsumerRabitMQAPI.Controllers
+{
+    public class RabitClientService : BackgroundService
+    {
+        private readonly ILogger _logger;
+        private IConnection _connection;
+        private IModel _channel;
+
+        public RabitClientService(ILoggerFactory loggerFactory)
+        {
+            this._logger = loggerFactory.CreateLogger<RabitClientService>();
+            InitRabbitMQ();
+        }
+
+        private void InitRabbitMQ()
+        {
+            var factory = new ConnectionFactory { HostName = "localhost", Port = 31672 };
+
+            // create connection  
+            _connection = factory.CreateConnection();
+
+            // create channel  
+            _channel = _connection.CreateModel();
+
+            //_channel.ExchangeDeclare("demo.exchange", ExchangeType.Topic);
+            _channel.QueueDeclare("TaskQueue", true, false, false, null);
+            // _channel.QueueBind("demo.queue.log", "demo.exchange", "demo.queue.*", null);
+            // _channel.BasicQos(0, 1, false);
+
+            _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (ch, ea) =>
+            {
+                // received message  
+                var content = System.Text.Encoding.UTF8.GetString(ea.Body.ToArray());
+
+                // handle the received message  
+                HandleMessage(content);
+                _channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+            consumer.Shutdown += OnConsumerShutdown;
+            consumer.Registered += OnConsumerRegistered;
+            consumer.Unregistered += OnConsumerUnregistered;
+            consumer.ConsumerCancelled += OnConsumerConsumerCancelled;
+
+            _channel.BasicConsume("greetings", false, consumer);
+            return Task.CompletedTask;
+        }
+
+        private void HandleMessage(string content)
+        {
+            // we just print this message   
+            _logger.LogInformation($"consumer received {content}");
+        }
+
+        private void OnConsumerConsumerCancelled(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerUnregistered(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerRegistered(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerShutdown(object sender, ShutdownEventArgs e) { }
+        private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e) { }
+
+        public override void Dispose()
+        {
+            _channel.Close();
+            _connection.Close();
+            base.Dispose();
+        }
+    }
+}
